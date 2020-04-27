aeaf1e1f766921c90138f22215fdbe0f2618ffab
Hruday chand Katakam <57784680+hrudaychandk@users.noreply.github.com>
2020-04-27 02:00:26 -0400

    Updated sleep time for child page

--- src/test/java/org/hruday/WebAppTest.java
+++ src/test/java/org/hruday/WebAppTest.java
@@ -36,7 +36,7 @@ public class WebAppTest {
     private void testHomePage() throws Exception{
         assertEquals("superServe", driver.getTitle());
         driver.findElement(By.xpath("/html/body/p/a")).click();
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         assertEquals("superServe Greetings", driver.getTitle());
         String greetingsBody = driver.findElement(By.xpath("/html/body/p")).getText();
         assertEquals("Hello, World!", greetingsBody);
